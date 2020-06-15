a42ad90e78572d924dbbde5e19287dd3c4cf07e8
Matt Reiss <mvr5256@gmail.com>
2020-06-15 16:39:01 -0400

    Add date to startup script logs

--- startup.sh
+++ startup.sh
@@ -2,21 +2,23 @@
 
 PID_FILE=".pid"
 
+DATE=$(date)
+
 function startup() {
   git pull origin master
   python install --upgrade -r requirements.txt
   nohup python bot.py > ./output.txt 2>&1 &
   echo $! > $PID_FILE
-  echo "Started new Discord Bot process"
+  echo "$DATE - Started new Discord Bot process"
 }
 
 if [ -f "$PID_FILE" ]; then
   PROCESS=$(echo "$PID_FILE")
 
   if pgrep -F $PID_FILE; then
-      echo "Process already running"
+      echo "$DATE - Process already running"
   else
-      echo "Process not running, starting up..."
+      echo "$DATE - Process not running, starting up..."
       startup
   fi
 else
