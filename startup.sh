3ecd6046192f3a8a9f40c2c9cbdb1406683f36c7
Matt Reiss <mvr5256@gmail.com>
2020-06-15 16:56:21 -0400

    Add newlines to echo statements

--- startup.sh
+++ startup.sh
@@ -5,20 +5,20 @@ PID_FILE=".pid"
 DATE=$(date)
 
 function startup() {
-  echo "$DATE - $(git pull origin master)\n"
-  echo "$DATE - $(pip install --upgrade -r requirements.txt)\n"
-  echo "$DATE - $(nohup python bot.py > ./output.txt 2>&1 &)\n"
+  echo -e "$DATE - $(git pull origin master)\n"
+  echo -e "$DATE - $(pip install --upgrade -r requirements.txt)\n"
+  echo -e "$DATE - $(nohup python bot.py > ./output.txt 2>&1 &)\n"
   echo $! > $PID_FILE
-  echo "$DATE - Started new Discord Bot process"
+  echo -e "$DATE - Started new Discord Bot process\n"
 }
 
 if [ -f "$PID_FILE" ]; then
   PROCESS=$(echo "$PID_FILE")
 
   if pgrep -F $PID_FILE; then
-      echo "$DATE - Process already running"
+      echo -e "$DATE - Process already running\n"
   else
-      echo "$DATE - Process not running, starting up..."
+      echo -e "$DATE - Process not running, starting up...\n"
       startup
   fi
 else
